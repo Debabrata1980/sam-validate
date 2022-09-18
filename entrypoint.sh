8690e69e0a77b15432e84eaacd0a474973c07b1e
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2022-09-18 15:16:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 template_file="$1"
 region="$2"
 echo "Sam validation started.."
-sam validate -t template_file --region region
+sam validate -t ${template_file} --region ${region}
 if [ $? -ne 0 ]; then
   echo "SAM validate error"
   exit 1
